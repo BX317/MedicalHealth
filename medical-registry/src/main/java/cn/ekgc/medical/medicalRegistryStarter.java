51771615f448b7de62a4ce91fa6c84ffc4a7e9ea
暖赫 <bai99727@163.com>
2021-10-20 21:17:12 +0800

    1、修改 medical-parent 项目 pom.xml 文件。 2、创建完成 medical-registry 项目

---
+++ medical-registry/src/main/java/cn/ekgc/medical/medicalRegistryStarter.java
@@ -0,0 +1,20 @@
+package cn.ekgc.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <b>智慧医疗信息平台 - 注册中心启动类</b>
+ * @author 暖赫
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class medicalRegistryStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(medicalRegistryStarter.class, args);
+	}
+}
